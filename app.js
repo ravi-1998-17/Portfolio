18ab1258a315cc2a661e9dfee0e6c6388e783a9d
Ravi Suthar <ravisuthar1711@gmail.com>
2024-10-08 22:01:21 +0530

    Under Construction

---
+++ app.js
@@ -0,0 +1,35 @@
+let logohover = document.querySelector('header nav .logo #hover-img');
+let diveIn  = document.querySelector('#hover');
+let cardhover  = document.querySelectorAll('.card');
+
+// Nav Brand Profile Picture 
+logohover.addEventListener('mouseover', ()=>{
+    logohover.src = 'images/profile-2.png';
+})
+logohover.addEventListener('mouseout', ()=>{
+    logohover.src = 'images/profile-1.png';
+})
+
+// Dive in icon turn into dark
+diveIn.addEventListener('mouseover', ()=>{
+    diveIn.setAttribute('class', 'bi bi-caret-down-fill');
+})
+
+diveIn.addEventListener('mouseout', ()=>{
+    diveIn.setAttribute('class', 'bi bi-caret-down');
+})
+
+// Card Shadow adding
+for (const card of cardhover) {
+    card.addEventListener('mouseover', () => {
+        card.classList.add('shadow');
+        console.log('hello')
+      });
+    card.addEventListener('mouseout', () => {
+    card.classList.remove('shadow');
+    });
+
+      
+}
+
+
